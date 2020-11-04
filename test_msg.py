87d8896577b58bf60bdd5ef0c1cf50d9abfaa3ae
Lital Natan <deboxed360@gmail.com>
2020-11-04 11:53:30 +0200

    Better tests
    Signed-off-by: Lital Natan <deboxed360@gmail.com>

---
+++ test_msg.py
@@ -0,0 +1,15 @@
+﻿from wormhole.message import WormholeMessage
+
+
+class TestMessage(WormholeMessage):
+    def __init__(self, a):
+        self.a = a
+
+    def __repr__(self):
+        return f"TestMessage({self.a})"
+
+
+class TestReply:
+    def __init__(self, r1, r2):
+        self.r1 = r1
+        self.r2 = r2
